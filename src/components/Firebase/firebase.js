bda7ac6cbc2807c3b7e1271daaf71e3ecbb40454
John Soter <john.soter@outlook.com>
2019-08-24 17:32:59 -0700

    added signup authentication

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,34 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBqENN-cv1-DF0ZRPNL7nVDE43NS8Vz1HI",
+    authDomain: "liquorclub-4831f.firebaseapp.com",
+    databaseURL: "https://liquorclub-4831f.firebaseio.com",
+    projectId: "liquorclub-4831f",
+    storageBucket: "liquorclub-4831f.appspot.com",
+    messagingSenderId: "606395750090",
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+
+        this.auth = app.auth();
+    }
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+      this.auth.currentUser.updatePassword(password);
+}
+  
+export default Firebase;
